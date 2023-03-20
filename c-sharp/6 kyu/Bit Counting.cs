47621b1a160a2f0adbdf6b3f939883dc64db2dcc
oqo0 <oqqqqqqqqqqq@gmail.com>
2023-03-20 19:12:41 +0300

    Add 6 kyu Bit Counting.cs

---
+++ c-sharp/6 kyu/Bit Counting.cs
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/526571aae218b8ee490006f4/
+
+using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int CountBits(int n)
+  {
+    return Convert.ToString(n, 2).Count(x => x == '1');
+  }
+}

2f58236c5d999d988084e435cd3ea0e0ff7e606e
oqo0 <oqqqqqqqqqqq@gmail.com>
2023-03-20 19:13:13 +0300

    Add 6 kyu CreatePhoneNumber.cs

---
+++ c-sharp/6 kyu/CreatePhoneNumber.cs
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/525f50e3b73515a6db000b83/
+
+public class Kata
+{
+  public static string CreatePhoneNumber(int[] n)
+  {
+    return $"({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}";
+  }
+}
